87cf540fe93d817be8b3ed585264a755fac53395
HUGER <tho.huger@btssio.lan>
2026-03-25 11:25:44 +0100

    ajout de tous

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+<footer id="footer" class="footer accent-background">
+
+<div class="container">
+  <div class="copyright text-center ">
+    <p>© <span>Copyright</span> <strong class="px-1 sitename">Théo Huger</strong> <span>All Rights Reserved</span></p>
+  </div>
+  <div class="social-links d-flex justify-content-center">
+    <a href=""><i class="bi bi-twitter-x"></i></a>
+    <a href=""><i class="bi bi-facebook"></i></a>
+    <a href=""><i class="bi bi-instagram"></i></a>
+    <a href=""><i class="bi bi-linkedin"></i></a>
+  </div>
+</div>
+
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,27 @@
+<header id="header" class="header d-flex align-items-center sticky-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1 class="sitename">Portfolio</h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li><a href="#hero" class="active">Accueil<br></a></li>
+          <li><a href="#about">À propos de moi</a></li>
+          <li><a href="#resume">Mon Parcours</a></li>
+          <li><a href="#services">Mes Compétences</a></li>
+          <li><a href="#portfolio">Mes Certifications</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <?php
+            if
+
+
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+
+    </div>
+</header>
