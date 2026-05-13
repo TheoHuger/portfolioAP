09e353a437e5fb85963a34ec37cb7e5c8ba1a6b8
HUGER <tho.huger@btssio.lan>
2026-05-13 10:37:56 +0200

    changement menu

--- includes/header.php
+++ includes/header.php
@@ -10,16 +10,29 @@
       <nav id="navmenu" class="navmenu">
         <ul>
           <li><a href="#hero" class="active">Accueil<br></a></li>
-          <li><a href="#about">À propos de moi</a></li>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            À propos de moi
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#about">Présentation</a></li>
+            <?php if (isset($_SESSION["login"])) { ?>
+              <li><a class="dropdown-item" href="#resume">Mon Parcours</a></li>
+              <li><a class="dropdown-item" href="#portfolio">Mes Certifications</a></li>
+            <?php } ?>
+          </ul>
+        </li>
+
+
+
           <?php if (!isset($_SESSION["login"])) { ?>
             <li class="nav-item"><a class="nav-link" href="index.php?uc=creationcompte&action=creation">Créer un compte</li>
             <li class="nav-item"><a class="nav-link" href="index.php?uc=authentification&action=se_connecter">Se connecter<span class="bi bi-box-arrow-in-left"></span></a></li>
           <?php } ?>
           
           <?php if (isset($_SESSION["login"])) { ?>
-                <li><a href="#resume">Mon Parcours</a></li>
                 <li><a href="#services">Mes Compétences</a></li>
-                <li><a href="#portfolio">Mes Certifications</a></li>
                 <li><a href="#stages">Mes Stages</a></li>
                 <li><a href="#contact">Contact</a></li>
                 <ul class="navbar-nav ms-auto">
